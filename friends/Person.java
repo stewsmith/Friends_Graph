5efab0c335698a23e05d2eb3f2cfcf0519101101
stewsmith <stewartlsmith12@gmail.com>
2012-11-29 11:32:06 -0500

    Update friends/Person.java

--- friends/Person.java
+++ friends/Person.java
@@ -5,22 +5,24 @@ import java.util.*;
 public class Person{
 	public String name;
 	public String school;
-	//public ArrayList<Person> friendList;
+	public Person next;
+	public ArrayList<Person> friendList;
 	
-	public Person(String name, String school){	//ArrayList<Person> friendList
+	public Person(String name, String school, Person next){	//ArrayList<Person> friendList
 		this.name = name;
 		this.school = school;
+		this.next = next;
 		//this.friendList = friendList;
 	}
 }
 
 
-class friendList{
-	Person person;
-	friendList next;
-	
-	public friendList(String name, String school, friendList next){
-		person = new Person(name, school);
-		this.next = next;
-	}
-}
+//class friendList{
+//	Person person;
+//	friendList next;
+//	
+//	public friendList(String name, String school, friendList next){
+//		person = new Person(name, school);
+//		
+//	}
+//}
