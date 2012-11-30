45349228ae4afc5b293fc4a3ea778ff86d2ee423
stewsmith <stewartlsmith12@gmail.com>
2012-11-29 21:45:42 -0500

    Update friends/Graph_Test.java

--- friends/Graph_Test.java
+++ friends/Graph_Test.java
@@ -58,7 +58,7 @@ public class Graph_Test {
         System.out.print("Enter the name of the friendship file => ");
         br2 = new BufferedReader(new FileReader(br1.readLine()));
        
-        int count = Integer.parseInt(br2.readLine())+1;	//number of people
+        int count = Integer.parseInt(br2.readLine())+1;    //number of people
         String[] personBuk = new String[count];	
       
         for(int i=0; i<count; i++){			//put the people lines in an array
@@ -74,8 +74,13 @@ public class Graph_Test {
         	line = br2.readLine();
         	friendsBuk.add(line);
         }
-        Person[] graph = new Person[count];  //ANS: Person[count] is the syntax to make an person array with size count
-        graph = build(personBuk, friendsBuk);
+        
+        Person[] graph = new Person[count];
+       
+        graph = build(personBuk, friendsBuk);		//FUCK HOW DOES THIS SYNTAX WORK?!?!
+        
+        System.out.println(graph);
+        
        
         int choice = getChoice();
         while (choice != QUIT) {
@@ -94,4 +99,62 @@ public class Graph_Test {
         }
        
     }
+    
+    public static Person[] build(String[] people, ArrayList<String> friends){
+ 	   Person[] zoo = new Person[people.length];
+ 	   for(int i=0; i<people.length; i++){		// go through names and make them people
+ 		   String raw = people[i];
+ 		   String school=null;
+ 		   String name;
+ 		   
+ 		   name = raw.substring(0, raw.indexOf("|"));	//get name until '|'
+ 		   System.out.println("name: "+name);
+ 		   if(raw.charAt(raw.indexOf("|")+1)=='y'){				//attends school?
+ 			   school=raw.substring(raw.lastIndexOf('|')+1, raw.length());	//get the schoolname
+ 			   System.out.println("school: " + school);
+ 		   }
+ 		  
+ 		   
+ 		   Person body = new Person(name, school, null);	//create a new person
+ 		   zoo[i]= body;							//put him in the zoo
+ 	   }
+ 	   
+ 	   for(int j=0; j<friends.size()-1; j++){		//go through the friends
+ 		   String raw = friends.get(j);
+ 		   String first = raw.substring(0, raw.indexOf('|'));		//the first friend name
+ 		   String second = raw.substring(raw.indexOf('|')+1);			//the second friend name
+ 		   Person firstPerson = new Person(null, null, null);
+ 		   Person secondPerson = new Person(null, null, null);
+ 		   boolean firstAssigned = false;
+ 		   boolean secAssigned = false;
+
+ 		   for(int k=0; k<zoo.length; k++){		//searching array --- there is probably a better way
+
+ 			   if (zoo[k].name.equalsIgnoreCase(first)){		//the names match
+ 				   firstPerson = zoo[k];
+ 				   firstAssigned=true;
+ 			   }
+ 			   if(zoo[k].name.equalsIgnoreCase(second)){
+ 				   secondPerson = zoo[k];
+ 				   secAssigned=true;
+ 			   }
+ 			   if(firstAssigned && secAssigned){
+ 				   if(firstPerson.next != null){		//attach the second friend to the first without losing old friend chain
+ 					   Person temp = firstPerson.next;
+ 					   firstPerson.next = secondPerson;
+ 					   secondPerson.next = temp;
+ 				   }else firstPerson.next = secondPerson;
+ 				   if(secondPerson.next != null){		//attach the first friend to the second without losing old friend chain
+ 					   Person temp = secondPerson.next;
+ 					   secondPerson.next = firstPerson;
+ 					   firstPerson.next = temp;
+ 				   }else secondPerson.next = firstPerson;
+ 			   }
+ 		   }
+ 				  
+ 	   }
+ 	   return zoo;		//temporary return
+    }
+
+	
 }
