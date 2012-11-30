e8f5162df9cf43f9dbf92afe5255843adb2fef43
stewsmith <stewartlsmith12@gmail.com>
2012-11-30 17:00:09 -0500

    Update friends/Graph_Test.java

--- friends/Graph_Test.java
+++ friends/Graph_Test.java
@@ -26,13 +26,7 @@ public class Graph_Test {
 		System.out.print("\tEnter choice # => ");
 		return (Integer.parseInt(br1.readLine()));
 	}
-
-	public static void subgraph()
-			throws IOException {
-		System.out.print("Enter the name of the school => ");
-		String sc = stdin.nextLine();
-
-	}
+	
 
 	public static void shortest()
 			throws IOException {
@@ -59,9 +53,9 @@ public class Graph_Test {
 		br2 = new BufferedReader(new FileReader(br1.readLine()));
 
 		int count = Integer.parseInt(br2.readLine())+1;	//number of people
-		String[] personBuk = new String[count];	
+		String[] personBuk = new String[count-1];	
 
-		for(int i=0; i<count; i++){			//put the people lines in an array
+		for(int i=0; i<count-1; i++){			//put the people lines in an array
 			String line= br2.readLine();
 			personBuk[i]= line;
 		}
@@ -76,19 +70,18 @@ public class Graph_Test {
 		}
 
 		Person[] graph = new Person[count];
-
 		graph = build(personBuk, friendsBuk);		//FUCK HOW DOES THIS SYNTAX WORK?!?!
-
-		System.out.println(graph);
-
-
+		
 		int choice = getChoice();
 		while (choice != QUIT) {
 			if (choice < 1 || choice > QUIT) {
 				System.out.println("\tIncorrect choice " + choice);
 			} else {
 				switch (choice) {
-				case STUDENTS: subgraph(); break;
+				case STUDENTS: 
+					System.out.print("Enter the name of the school => ");
+					String sc = stdin.nextLine();
+					subgraph(sc, graph); break;
 				case SHORT: shortest(); break;
 				case CLIQUE: cliques(); break;
 				case CONNECT: connectors(); break;    //revise
@@ -97,7 +90,6 @@ public class Graph_Test {
 			}
 			choice = getChoice();
 		}
-
 	}
 
 	public static Person[] build(String[] people, ArrayList<String> friends){
@@ -134,7 +126,7 @@ public class Graph_Test {
 				if (zoo[k].name.equalsIgnoreCase(second)){
 					secDex.friendNum=k;
 					secMatched=true;
-				}
+				}	
 				if(firstMatched && secMatched){
 					Friendex temp1 = zoo[firstDex.friendNum].front;		//keep the chain that is already attached
 					Friendex temp2 = zoo[secDex.friendNum].front;
@@ -148,6 +140,33 @@ public class Graph_Test {
 				}
 			}
 		}
-		return zoo;		//temporary return
+		return zoo;		
+	}
+	public static ArrayList<Person> subgraph(String school, Person[] zoo){
+		ArrayList<Person> schoolZoo = new ArrayList<>();
+		for(int i=0; i< zoo.length; i++){			//go through all people in zoo----linear
+			if(zoo[i].school != null && zoo[i].school.equals(school)){		//if their school matches
+				Person tempPer = new Person(zoo[i].name, zoo[i].school, zoo[i].front);	//copy zoo person
+				schoolZoo.add(tempPer);				//add the person to the arrayList
+			}
+		}
+		for(int k=0; k<schoolZoo.size(); k++){		//go through schoolZoo
+			Person student =  schoolZoo.get(k);		//person in the schoolZoo
+			Friendex ptr = student.front;
+			Friendex prev = null;
+			while(ptr != null){		//go through the attached chain
+				if(zoo[ptr.friendNum].school != null && !zoo[ptr.friendNum].school.equals(school)){
+					if(prev==null){			//first person doesn't attend target school
+						student.front = ptr.next;
+					}else{
+						prev.next = ptr.next;		//cut chains to non-attending person
+					}
+				}else{
+					prev=ptr; 		//student attends--- don't modify
+				}
+				ptr=ptr.next;
+			}
+		}
+		return schoolZoo;
 	}
 }
