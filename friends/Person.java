eb3793ccb0dc0e2187edf1c35d0340d3316b035f
stewsmith <stewartlsmith12@gmail.com>
2012-12-01 14:54:03 -0500

    Update friends/Person.java

--- friends/Person.java
+++ friends/Person.java
@@ -1,28 +1,18 @@
 package friends;
 
-import java.util.*;
-
 public class Person{
 	public String name;
 	public String school;
-	public Person next;
-	public ArrayList<Person> friendList;
+	public Friendex front;
+	public boolean visited;
+	public int zooIndex;
 	
-	public Person(String name, String school, Person next){	//ArrayList<Person> friendList
+	public Person(String name, String school, Friendex front, boolean visited, int zooIndex){	//ArrayList<Person> friendList
 		this.name = name;
 		this.school = school;
-		this.next = next;
-		//this.friendList = friendList;
+		this.front= front;
+		this.visited = visited;
+		this.zooIndex = zooIndex;
 	}
 }
 
-
-//class friendList{
-//	Person person;
-//	friendList next;
-//	
-//	public friendList(String name, String school, friendList next){
-//		person = new Person(name, school);
-//		
-//	}
-//}
